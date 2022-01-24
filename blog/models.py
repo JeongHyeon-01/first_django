7d3228f54d3c818c2c21dc80b59640c8b1d19b63
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-24 13:29:31 +0900

    FBA blog posting

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+
+    def get_absolute_url(self):
+        return f"/blog/{self.pk}/"
--- blog/urls.py
+++ blog/urls.py
@@ -17,4 +17,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
+    path('<int:pk>/',views.single_post_page),
 ]
