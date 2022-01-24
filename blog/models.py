b69f66c236427686a912fe584512c5b413a8b452
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-24 13:04:21 +0900

    create Post models and timezone

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
+from email import contentmanager
+from pyexpat import model
+from turtle import title
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #auth
+
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
